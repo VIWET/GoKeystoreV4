8b9a435795aa57b95779d247ec34ddcba6d75393
Dmitry Sidorov <viwet42@gmail.com>
2024-11-15 19:41:53 +0100

    feat(crypto): add json decoding function

--- crypto.go
+++ crypto.go
@@ -2,19 +2,9 @@ package gokeystorev4
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 )
-
-type ModuleFunction struct {
-	Function string `json:"function"`
-}
-
-// Generic crypto module
-type Module[T any] struct {
-	ModuleFunction
-	Params  T   `json:"params"`
-	Message Hex `json:"message"`
-}
 
 // Crypto modules of keystore
 type Crypto struct {
@@ -71,4 +61,80 @@ func (crypto *Crypto) Decrypt(password []byte) ([]byte, error) {
 	}
 
 	return secret, nil
+}
+
+// Generic crypto module
+type Module[T any] struct {
+	Function string `json:"function"`
+	Params   T      `json:"params"`
+	Message  Hex    `json:"message"`
+}
+
+// ModuleJSON representation
+type ModuleJSON struct {
+	Function string          `json:"function"`
+	Params   json.RawMessage `json:"params"`
+	Message  Hex             `json:"message"`
+}
+
+// CryptoJSON representation
+type CryptoJSON struct {
+	KDF      *ModuleJSON `json:"kdf"`
+	Cipher   *ModuleJSON `json:"cipher"`
+	Checksum *ModuleJSON `json:"checksum"`
+}
+
+// impl json.Unmarshaler for Crypto
+func (c *Crypto) UnmarshalJSON(data []byte) error {
+	var crypto CryptoJSON
+	if err := json.Unmarshal(data, &crypto); err != nil {
+		return err
+	}
+
+	var err error
+	switch crypto.KDF.Function {
+	case ScryptName:
+		c.KDF, err = unmarshalModule[KDFunction](new(Scrypt), crypto.KDF)
+	case PBKDF2Name:
+		c.KDF, err = unmarshalModule[KDFunction](new(PBKDF2), crypto.KDF)
+	default:
+		return fmt.Errorf("unknown key derivation function: %s", crypto.KDF.Function)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal KDF module: %w", err)
+	}
+
+	switch crypto.Cipher.Function {
+	case AES128Name:
+		c.Cipher, err = unmarshalModule[CipherFunction](new(AES128), crypto.Cipher)
+	default:
+		return fmt.Errorf("unknown cipher function: %s", crypto.Cipher.Function)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal Cipher module: %w", err)
+	}
+
+	switch crypto.Checksum.Function {
+	case Sha256Name:
+		c.Checksum, err = unmarshalModule[ChecksumFunction](new(Sha256), crypto.Checksum)
+	default:
+		return fmt.Errorf("unknown checksum function: %s", crypto.Checksum.Function)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal Checksum module: %w", err)
+	}
+
+	return nil
+}
+
+func unmarshalModule[T any](params T, module *ModuleJSON) (*Module[T], error) {
+	if err := json.Unmarshal(module.Params, params); err != nil {
+		return nil, err
+	}
+
+	return &Module[T]{
+		Function: module.Function,
+		Params:   params,
+		Message:  module.Message,
+	}, nil
 }
