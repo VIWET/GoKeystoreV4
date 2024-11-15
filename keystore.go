a74f8395e0dd4817315d8458ee1a353a1b985ff9
Dmitry Sidorov <viwet42@gmail.com>
2024-11-15 19:49:52 +0100

    feat(keystore): add keystore Encrypt/Decrypt functions

--- keystore.go
+++ keystore.go
@@ -12,3 +12,29 @@ type Keystore struct {
 	UUID        uuid.UUID `json:"uuid"`
 	Version     uint64    `json:"version"`
 }
+
+func newKeystore(path string) *Keystore {
+	return &Keystore{
+		Crypto:  DefaultCrypto(),
+		Path:    path,
+		UUID:    uuid.New(),
+		Version: KeystoreVersion,
+	}
+}
+
+func Encrypt(secret []byte, password, path string, opts ...KeystoreOption) (*Keystore, error) {
+	keystore := newKeystore(path)
+	if err := KeystoreOptions(opts).Apply(keystore); err != nil {
+		return nil, err
+	}
+
+	if err := keystore.Crypto.Encrypt(secret, ProcessPassword(password)); err != nil {
+		return nil, err
+	}
+
+	return keystore, nil
+}
+
+func Decrypt(keystore *Keystore, password string) ([]byte, error) {
+	return keystore.Crypto.Decrypt(ProcessPassword(password))
+}
