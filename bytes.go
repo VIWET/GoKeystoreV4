d804245c07356ded5ef65edc437d2a8fee0510d8
Dmitry Sidorov <viwet42@gmail.com>
2024-11-15 14:46:29 +0100

    feat(bytes): add Bytes helper functions and types

---
+++ bytes.go
@@ -0,0 +1,54 @@
+package gokeystorev4
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"strings"
+)
+
+// RandomBytse generates random bytes sequence of given length
+func RandomBytes(length int) []byte {
+	random := make([]byte, length)
+	if _, err := rand.Read(random); err != nil {
+		panic(err)
+	}
+	return random
+}
+
+// Hex string
+type Hex []byte
+
+// Salt
+type Salt = Hex
+
+// imlp json.Marshaler for Hex
+func (h Hex) MarshalJSON() ([]byte, error) {
+	hexstr := hex.EncodeToString(h)
+	if len(h) > 0 {
+		hexstr = "0x" + hexstr
+	}
+
+	return json.Marshal(&hexstr)
+}
+
+// imlp json.Unmarshaler for Hex
+func (h *Hex) UnmarshalJSON(data []byte) error {
+	var (
+		hexstr string
+		err    error
+	)
+
+	err = json.Unmarshal(data, &hexstr)
+	if err != nil {
+		return err
+	}
+
+	hexstr = strings.TrimPrefix(hexstr, "0x")
+	*h, err = hex.DecodeString(hexstr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
